cc5119bac60631b035ecde4195eac6448569149e
Shailendra-Jadeja <jadejashyam@gmail.com>
2021-08-11 12:25:56 +1200

    first commit

---
+++ Data/Migrations/20210807232156_initialsetup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JokesWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Joke",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    JokeQuestion = table.Column<int>(nullable: false),
+                    JokeAnswer = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Joke", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Joke");
+        }
+    }
+}
---
+++ Data/Migrations/20210808001509_initialsetup1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JokesWebApp.Data.Migrations
+{
+    public partial class initialsetup1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeQuestion",
+                table: "Joke",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JokeAnswer",
+                table: "Joke",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "JokeQuestion",
+                table: "Joke",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "JokeAnswer",
+                table: "Joke",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
